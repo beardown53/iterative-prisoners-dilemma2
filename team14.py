5085dab2485befc73647eb582ae898addfaf0d5e
beardown53 <oscarbear53@gmail.com>
2016-11-03 12:49:12 -0700

    Update team14.py

--- team14.py
+++ team14.py
@@ -11,6 +11,13 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
+    if len(my_history)==0: 
+        return 'b'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' 
+    else:
+        return 'b' 
+    
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
@@ -26,7 +33,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -65,4 +72,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')                     
